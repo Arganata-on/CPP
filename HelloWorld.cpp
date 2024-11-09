ec75c23338b8b970c6988b2a8e3e7addd99aa747
Handika Rado Arganata <arganata.on@gmail.com>
2024-11-10 02:56:58 +0700

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+// Membuat project untuk menampilkan tulisan “Hello World <nama mahasiswa>”
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+  string namaMahasiswa;
+  cout << "Masukkan namamu: ";
+  getline(cin, namaMahasiswa);
+  cout << "Hello world " << namaMahasiswa;
+  return 0;
+}
